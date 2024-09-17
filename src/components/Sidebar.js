291c7e11944844dce95bd883935d419bf9955cbb
Anjali <anjalibhatkal03@gmail.com>
2024-09-17 23:47:13 +0530

    added sidebar structure

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,8 +1,50 @@
 import React from "react";
 import "../components/myStyles.css";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import PersonAddIcon from "@mui/icons-material/PersonAdd";
+import GroupAddIcon from "@mui/icons-material/GroupAdd";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import NightlightIcon from "@mui/icons-material/Nightlight";
+import SearchIcon from "@mui/icons-material/Search";
+import { IconButton } from "@mui/material";
+import ConversationsItem from "./ConversationsItem";
 
 function Sidebar() {
-  return <div className="sidebar-container">s</div>;
+  return (
+    <div className="sidebar-container">
+      <div className="sb-header">
+        <div>
+          <IconButton>
+            <AccountCircleIcon />
+          </IconButton>
+        </div>
+        <div>
+          <IconButton>
+            <PersonAddIcon />
+          </IconButton>
+          <IconButton>
+            <GroupAddIcon />
+          </IconButton>
+          <IconButton>
+            <AddCircleIcon />
+          </IconButton>
+          <IconButton>
+            <NightlightIcon />
+          </IconButton>
+        </div>
+      </div>
+      <div className="sb-search">
+        <IconButton>
+          <SearchIcon />
+        </IconButton>
+
+        <input type="text" placeholder="Search" className="search-box" />
+      </div>
+      <div className="sb-conversations">
+        <ConversationsItem />
+      </div>
+    </div>
+  );
 }
 
 export default Sidebar;
