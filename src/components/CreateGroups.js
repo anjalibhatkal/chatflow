43d681e2bb650f2be03257cede8f8e702ae01fa6
Anjali <anjalibhatkal03@gmail.com>
2024-09-22 22:01:01 +0530

    basic animations added

--- src/components/CreateGroups.js
+++ src/components/CreateGroups.js
@@ -2,22 +2,30 @@ import React from "react";
 import DoneIcon from "@mui/icons-material/Done";
 import { IconButton } from "@mui/material";
 import { useSelector } from "react-redux";
+import { motion, AnimatePresence } from "framer-motion";
 
 function CreateGroups() {
   const lightTheme = useSelector((state) => {
     return state.themeKey;
   });
   return (
-    <div className={"createGroups-container" + (lightTheme ? "" : " dark")}>
-      <input
-        type="text"
-        placeholder="Enter Group Name"
-        className={"search-box" + (lightTheme ? "" : " dark")}
-      />
-      <IconButton>
-        <DoneIcon className={"icon" + (lightTheme ? "" : " dark")} />
-      </IconButton>
-    </div>
+    <AnimatePresence>
+      <motion.div
+        initial={{ scale: 0.9, opacity: 0 }}
+        animate={{ scale: 1, opacity: 1 }}
+        transition={"anticipate"}
+        className={"createGroups-container" + (lightTheme ? "" : " dark")}
+      >
+        <input
+          type="text"
+          placeholder="Enter Group Name"
+          className={"search-box" + (lightTheme ? "" : " dark")}
+        />
+        <IconButton>
+          <DoneIcon className={"icon" + (lightTheme ? "" : " dark")} />
+        </IconButton>
+      </motion.div>
+    </AnimatePresence>
   );
 }
 
--- src/components/Groups.js
+++ src/components/Groups.js
@@ -3,6 +3,7 @@ import logo from "./../assets/chatflow.png";
 import { IconButton } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 import { useSelector } from "react-redux";
+import { AnimatePresence, motion } from "framer-motion";
 
 function Groups() {
   const lightTheme = useSelector((state) => {
@@ -10,66 +11,89 @@ function Groups() {
   });
 
   return (
-    <div className="list-container">
-      <div className={"ug-header" + (lightTheme ? "" : " dark")}>
-        <img
-          src={logo}
-          alt="Logo"
-          style={{ height: "2.5rem", width: "2.5rem" }}
-        />
-        <p className={"ug-title" + (lightTheme ? "" : " dark")}>
-          Available Groups
-        </p>
-      </div>
-      <div className={"sb-search" + (lightTheme ? "" : " dark")}>
-        <IconButton>
-          <SearchIcon className={"icon" + (lightTheme ? "" : " dark")} />
-        </IconButton>
-        <input
-          type="text"
-          placeholder="Search"
-          className={"search-box" + (lightTheme ? "" : " dark")}
-        />
-      </div>
-      <div className="ug-list">
-        <div className={"ug-item" + (lightTheme ? "" : " dark")}>
-          <p className={"convo-icon" + (lightTheme ? "" : " dark-outer")}>T</p>
-          <p className={"convo-title" + (lightTheme ? "" : " dark")}>
-            Text User
+    <AnimatePresence>
+      <motion.div
+        initial={{ scale: 0.9, opacity: 0 }}
+        animate={{ scale: 1, opacity: 1 }}
+        transition={"anticipate"}
+        className="list-container"
+      >
+        <div className={"ug-header" + (lightTheme ? "" : " dark")}>
+          <img
+            src={logo}
+            alt="Logo"
+            style={{ height: "2.5rem", width: "2.5rem" }}
+          />
+          <p className={"ug-title" + (lightTheme ? "" : " dark")}>
+            Available Groups
           </p>
         </div>
-        <div className={"ug-item" + (lightTheme ? "" : " dark")}>
-          <p className={"convo-icon" + (lightTheme ? "" : " dark-outer")}>T</p>
-          <p className={"convo-title" + (lightTheme ? "" : " dark")}>
-            Text User
-          </p>
+        <div className={"sb-search" + (lightTheme ? "" : " dark")}>
+          <IconButton>
+            <SearchIcon className={"icon" + (lightTheme ? "" : " dark")} />
+          </IconButton>
+          <input
+            type="text"
+            placeholder="Search"
+            className={"search-box" + (lightTheme ? "" : " dark")}
+          />
         </div>
-        <div className={"ug-item" + (lightTheme ? "" : " dark")}>
-          <p className={"convo-icon" + (lightTheme ? "" : " dark-outer")}>T</p>
-          <p className={"convo-title" + (lightTheme ? "" : " dark")}>
-            Text User
-          </p>
+        <div className="ug-list">
+          <motion.div
+            whileHover={{ scale: 1.01 }}
+            whileTap={{ scale: 0.98 }}
+            className={"ug-item" + (lightTheme ? "" : " dark")}
+          >
+            <p className={"convo-icon" + (lightTheme ? "" : " dark-outer")}>
+              T
+            </p>
+            <p className={"convo-title" + (lightTheme ? "" : " dark")}>
+              Text User
+            </p>
+          </motion.div>
+          <div className={"ug-item" + (lightTheme ? "" : " dark")}>
+            <p className={"convo-icon" + (lightTheme ? "" : " dark-outer")}>
+              T
+            </p>
+            <p className={"convo-title" + (lightTheme ? "" : " dark")}>
+              Text User
+            </p>
+          </div>
+          <div className={"ug-item" + (lightTheme ? "" : " dark")}>
+            <p className={"convo-icon" + (lightTheme ? "" : " dark-outer")}>
+              T
+            </p>
+            <p className={"convo-title" + (lightTheme ? "" : " dark")}>
+              Text User
+            </p>
+          </div>
+          <div className={"ug-item" + (lightTheme ? "" : " dark")}>
+            <p className={"convo-icon" + (lightTheme ? "" : " dark-outer")}>
+              T
+            </p>
+            <p className={"convo-title" + (lightTheme ? "" : " dark")}>
+              Text User
+            </p>
+          </div>
+          <div className={"ug-item" + (lightTheme ? "" : " dark")}>
+            <p className={"convo-icon" + (lightTheme ? "" : " dark-outer")}>
+              T
+            </p>
+            <p className={"convo-title" + (lightTheme ? "" : " dark")}>
+              Text User
+            </p>
+          </div>
+          <div className={"ug-item" + (lightTheme ? "" : " dark")}>
+            <p className={"convo-icon" + (lightTheme ? "" : " dark-outer")}>
+              T
+            </p>
+            <p className={"convo-title" + (lightTheme ? "" : " dark")}>
+              Text User
+            </p>
+          </div>
         </div>
-        <div className={"ug-item" + (lightTheme ? "" : " dark")}>
-          <p className={"convo-icon" + (lightTheme ? "" : " dark-outer")}>T</p>
-          <p className={"convo-title" + (lightTheme ? "" : " dark")}>
-            Text User
-          </p>
-        </div>
-        <div className={"ug-item" + (lightTheme ? "" : " dark")}>
-          <p className={"convo-icon" + (lightTheme ? "" : " dark-outer")}>T</p>
-          <p className={"convo-title" + (lightTheme ? "" : " dark")}>
-            Text User
-          </p>
-        </div>
-        <div className={"ug-item" + (lightTheme ? "" : " dark")}>
-          <p className={"convo-icon" + (lightTheme ? "" : " dark-outer")}>T</p>
-          <p className={"convo-title" + (lightTheme ? "" : " dark")}>
-            Text User
-          </p>
-        </div>
-      </div>
-    </div>
+      </motion.div>
+    </AnimatePresence>
   );
 }
 
