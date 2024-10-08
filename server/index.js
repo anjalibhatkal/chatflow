99f24e73b5a157868e2259b0003455aee66fc917
Anjali <anjalibhatkal03@gmail.com>
2024-10-08 21:36:47 +0530

    db connection

--- server/index.js
+++ server/index.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
 const app = express();
 dotenv.config();
 
+const connectDb = async () => {
+  try {
+    const connect = await mongoose.connect(process.env.MONGO_URI);
+    console.log("Server connected to db");
+  } catch (err) {
+    console.log("Server is not connected to db", err.message);
+  }
+};
+
+connectDb();
 app.get("/", (req, res) => {
   res.send("API is running");
 });
